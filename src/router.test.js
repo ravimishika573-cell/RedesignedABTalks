d78e342bd98d4e646534800ca8dc5ff15b652e75
ravimishika573-cell <ravimishika573@gmail.com>
2026-08-09 09:45:27 +0200

    Create router.test.js

---
+++ src/router.test.js
@@ -0,0 +1,12 @@
+import { route, currentPath } from "./router.js";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test("currentPath strips query string", () => {
+  globalThis.location = { hash: "#/dashboard?state=day1" };
+  assertEquals(currentPath(), "/dashboard");
+});
+
+Deno.test("currentPath defaults to / when hash is empty", () => {
+  globalThis.location = { hash: "" };
+  assertEquals(currentPath(), "/");
+});
